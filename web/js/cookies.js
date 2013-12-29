06964bfcf0b6af25c081b7d3be52fd0f5e11abf0
Paweł Winiecki <pawel.winiecki@gmail.com>
2013-12-29 23:42:07 +0100

    * changes in routing (post links changed to /{slug}) * added cookies policy popup * changes in robots.txt

---
+++ web/js/cookies.js
@@ -0,0 +1,49 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$('document').ready(function() {
+
+    var isAccepted = readCookie('cookies_acceptation');
+
+    if (isAccepted !== 'accepted') {
+        showCookiesDialog();
+    }
+
+});
+
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ')
+            c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0)
+            return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else
+        var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function showCookiesDialog() {
+    $('body').append('<div id="cookies-dialog" style="display: block;"><p>Ta strona używa Cookies. Dowiedz się więcej o celu ich używania - przeczytaj naszą politykę prywatności.<br /> Korzystając ze strony wyrażasz zgodę na używanie cookie, zgodnie z aktualnymi ustawieniami przeglądarki. <button id="cookies_accept" class="btn btn-sm btn-success">Akceptuję</button><br /><a href="http://nerdlab.pl/polityka-cookies/">Zapoznaj się z naszą polityką cookies</a></div></p>');
+    $('#cookies_accept').click(function() {
+        createCookie('cookies_acceptation','accepted',365);
+        $('#cookies-dialog').remove();
+    });
+}
+
